69cba8fb687a7939958b20a62e9e00b41fdf5bd2
Fabio Carlos <facarlos90@gmail.com>
2023-11-05 18:49:51 -0300

    Adicionando função pause nas músicas

--- main.js
+++ main.js
@@ -1,18 +1,51 @@
+let musicaTocando = null;
+
 function tocaMusica (idElementoMusica) {
-    document.querySelector(idElementoMusica).play();
+    const proximaMusica = document.querySelector(idElementoMusica); 
+
+    if (musicaTocando === null) {
+        proximaMusica.play();
+        musicaTocando=proximaMusica;
+        
+    }else if(musicaTocando != null && musicaTocando != proximaMusica){
+        musicaTocando.pause();
+        proximaMusica.play();
+        musicaTocando = proximaMusica;
+    } else {
+             console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
-const listaRecentes = document.querySelectorAll('.cartao__botao');
+function paraMusica () {
+    if (musicaTocando!= null) {
+        musicaTocando.pause();
+        musicaTocando=null
+    }
+}
 
+const listaRecentes = document.querySelectorAll('.botao--play');
+const listaPause = document.querySelectorAll('.botao--pause');
 
 for (let contador=0; contador < listaRecentes.length; contador++) {
 
-    const musica = listaRecentes[contador];
-    const musicaAtual = musica.classList[1];
+    const playMusica = listaRecentes[contador];
+    const musicaAtual = playMusica.classList[2];
     const idAudio = `#som_${musicaAtual}`;
 
-    musica.onclick = function () {
+    playMusica.onclick = function () {
         tocaMusica(idAudio);
+        listaPause[contador].style.display = 'block';
+        listaRecentes[contador].style.display = 'none';
     };
 }
 
+for (let contador=0; contador <listaPause.length; contador++){
+    
+    const pausaMusica = listaPause[contador];
+
+    pausaMusica.onclick = function () {
+        paraMusica();
+        listaRecentes[contador].style.display = 'block';
+        listaPause[contador].style.display = 'none';
+    };
+}
